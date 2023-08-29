11bab83ce94f0e5a519f826f6c07cca15ed94ab4
1barada <jionanrouk@gmail.com>
2023-08-29 14:19:49 +0300

    added caution

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,14 @@ function App() {
 
   return (
     <div className="App container">
+      <p 
+        className='container'
+        style={{textAlign: 'center'}}
+      >
+        <span style={{color: '#fbff00'}}>&#x26A0;</span> 
+        This app does not use DST(daylight save time)<br/>
+        This means that the time in some time zones will be differ from the real time (max difference: 2 hours).
+      </p>
       <Main/>
     </div>
   );
