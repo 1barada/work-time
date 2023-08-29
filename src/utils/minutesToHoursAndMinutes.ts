a398368ad2b1abb993a9b03c23d76f2eb113a150
1barada <jionanrouk@gmail.com>
2023-08-29 14:26:53 +0300

    fixed bag. incorrectly converted negative numbers

--- src/utils/minutesToHoursAndMinutes.ts
+++ src/utils/minutesToHoursAndMinutes.ts
@@ -1,13 +1,17 @@
 import ITimeForTimezones from "../models/ITimeForTimezones";
 
 export default function minutesToHoursAndMinutes(minutes: number): ITimeForTimezones {
+    if (minutes < 0) {
+        minutes = 1440 + minutes;
+    }
+
     let hours = Math.floor(minutes / 60);
     const minutesOutput = minutes - (hours * 60);
     
     if (hours > 23) {
         hours %= 24;
     }
-    
+
     return {
         hours: hours,
         minutes: minutesOutput
